0c2dac50d4fd2f096acffdcae3fa375b57d7cbb4
Fabricio-Porcel <faba_59@hotmail.com>
2023-11-24 13:45:37 +1300

    edited package.json ready to deploy

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const path = require ('path');
 const publicPath = path.resolve(__dirname , '../public');
 app.use(express.static(publicPath));
 
-app.listen(3002 , () => console.log("Servidor corriendo en el puerto http://localhost:3002"))
+const port = process.env.PORT || 3002;
+
+app.listen(port , () => console.log("Servidor corriendo en el puerto http://localhost:3002"))
 
 app.get('/' , (req , res) =>{
     res.sendFile(path.resolve(__dirname , './views/home.html'))
